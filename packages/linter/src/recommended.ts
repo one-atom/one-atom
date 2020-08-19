659e4d157d33d18a7ed11943a5138c2005f75e79
emil nordling <ekala001@gmail.com>
2020-08-19 23:26:25 +0200

    Adds back eslint:recommended

--- packages/linter/src/recommended.ts
+++ packages/linter/src/recommended.ts
@@ -16,6 +16,7 @@ export const recommended_eslint = {
     sourceType: 'module', // Allows for the use of imports
   },
   extends: [
+    'eslint:recommended', // Basic config
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
