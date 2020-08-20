6d5293f6278adf23504e78637061c121c97cdfe0
emil nordling <ekala001@gmail.com>
2020-08-20 23:03:04 +0200

    Updates button style

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -9,21 +9,55 @@ import { Size } from './size';
 export namespace Button {
   export interface Props extends BaseButton.Props, Size.Props, KiraPropType {}
 
-  const elements = {
+  const elements_shared = {
     button: styled(BaseButton.h)`
+      position: relative;
       height: 40px;
       padding: 0px 16px;
-      border-radius: 20px;
       box-sizing: border-box;
-      color: var(--kira_button_clr, #ffffff);
-      background-color: var(--kira_button_bg, #0099ff);
+      transition: background 150ms ease;
     `,
   };
 
-  export const h: React.FC<Props> = function __kira__button({ children, fluid, className, ...rest }) {
+  const elements = {
+    actionButton: styled(elements_shared.button)`
+      border-radius: 20px;
+      color: var(--kira-button-action-clr, #ffffff);
+      background: var(--kira-button-action-bg, linear-gradient(180deg, #0099ff, #008be8));
+
+      &:hover {
+        box-shadow: 0px 0.5px 1.5px rgba(54, 122, 246, 0.25);
+      }
+    `,
+    controlButton: styled(elements_shared.button)`
+      color: var(--kira-button-control-clr, var(--kira-text-color, #111));
+      border-radius: 5px;
+
+      &:hover {
+        background: var(--kira-button-control-bg, #e5e5e5);
+      }
+    `,
+  };
+
+  export const action: React.FC<Props> = function __kira__button_action({ children, fluid, className, type = 'action', ...rest }) {
     return (
       <Size.h fluid={fluid} className={className ?? ''}>
-        <elements.button {...rest}>{children}</elements.button>
+        <elements.actionButton {...rest}>{children}</elements.actionButton>
+      </Size.h>
+    );
+  };
+
+  export const alt: React.FC<Props> = function __kira__button_control({ children, fluid, className, type = 'action', ...rest }) {
+    return (
+      <Size.h fluid={fluid} className={className ?? ''}>
+        <elements.controlButton {...rest}>{children}</elements.controlButton>
+      </Size.h>
+    );
+  };
+  export const h: React.FC<Props> = function __kira__button({ children, fluid, className, type = 'action', ...rest }) {
+    return (
+      <Size.h fluid={fluid} className={className ?? ''}>
+        <elements_shared.button {...rest}>{children}</elements_shared.button>
       </Size.h>
     );
   };
