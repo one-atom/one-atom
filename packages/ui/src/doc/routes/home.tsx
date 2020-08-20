f84566e73a4baf4369d4c92ecb8431300171c72d
emil nordling <ekala001@gmail.com>
2020-08-20 23:32:56 +0200

    Adds more text to doc

--- packages/ui/src/doc/routes/home.tsx
+++ packages/ui/src/doc/routes/home.tsx
@@ -17,13 +17,33 @@ export namespace Home {
       <elements.container>
         <Page.h headerTitle='Almost before we knew it, we had left the ground.'>
           <Article.h>
-            <SectionArticle.section>Almost before we knew it, we had left the ground.</SectionArticle.section>
-            <SectionArticle.section>Almost before we knew it, we had left the ground.</SectionArticle.section>
             <SectionArticle.section>
               <Text.h1_plus>Almost before we knew it, we had left the ground.</Text.h1_plus>
               <SectionArticle.group>Almost before we knew it, we had left the ground.</SectionArticle.group>
             </SectionArticle.section>
-            <SectionArticle.section>Almost before we knew it, we had left the ground.</SectionArticle.section>
+            <SectionArticle.section>
+              <Text.h1>Almost before we knew it, we had left the ground.</Text.h1>
+              <SectionArticle.group>Almost before we knew it, we had left the ground.</SectionArticle.group>
+            </SectionArticle.section>
+            <SectionArticle.section>
+              <Text.h2>Almost before we knew it, we had left the ground.</Text.h2>
+              <SectionArticle.group>Almost before we knew it, we had left the ground.</SectionArticle.group>
+            </SectionArticle.section>
+            <SectionArticle.section>
+              <Text.h3>Almost before we knew it, we had left the ground.</Text.h3>
+              <SectionArticle.group>Almost before we knew it, we had left the ground.</SectionArticle.group>
+            </SectionArticle.section>
+            <SectionArticle.section>
+              <Text.h4>Almost before we knew it, we had left the ground.</Text.h4>
+              <SectionArticle.group>Almost before we knew it, we had left the ground.</SectionArticle.group>
+            </SectionArticle.section>
+            <SectionArticle.section>
+              <Text.body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+              </Text.body>
+              <SectionArticle.group>Almost before we knew it, we had left the ground.</SectionArticle.group>
+            </SectionArticle.section>
           </Article.h>
         </Page.h>
       </elements.container>
