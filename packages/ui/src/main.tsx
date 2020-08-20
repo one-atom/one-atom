8e0de063c056da75172219ec051d9195252f6c6b
emil nordling <ekala001@gmail.com>
2020-08-20 23:03:13 +0200

    Adds button doc

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -1,8 +1,9 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home } from './doc/routes/home';
 import { ResetCss } from './';
+import { Home } from './doc/routes/home';
+import { Buttons } from './doc/routes/buttons';
 
 ReactDOM.render(
   <Fragment>
@@ -10,6 +11,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home.h />} />
+        <Route path='/buttons' element={<Buttons.h />} />
       </Routes>
     </BrowserRouter>
   </Fragment>,
