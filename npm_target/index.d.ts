66fef16dd2cca29e336aeaf799a412e0848e07be
emil nordling <ekala001@gmail.com>
2020-11-13 23:48:17 +0100

    Fixes npm_target export

--- npm_target/index.d.ts
+++ npm_target/index.d.ts
@@ -1,3 +1,6 @@
-// One Atom
-
-export = 'one atom';
+export * from './dist/application_state/mod';
+export * from './dist/id/mod';
+export * from './dist/instantiation/mod';
+export * from './dist/logger/mod';
+export * from './dist/miscellaneous_modules/mod';
+export * from './dist/ui_kit/mod';
--- npm_target/run.js
+++ npm_target/run.js
@@ -21,9 +21,4 @@ var __exportStar =
     for (var p in m) if (p !== 'default' && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
   };
 Object.defineProperty(exports, '__esModule', { value: true });
-__exportStar(require('./dist/application_state/mod.js'), exports);
-__exportStar(require('./dist/id/mod'), exports);
-__exportStar(require('./dist/instantiation/mod'), exports);
-__exportStar(require('./dist/logger/mod'), exports);
-__exportStar(require('./dist/miscellaneous_modules/mod'), exports);
-__exportStar(require('./dist/ui_kit/mod'), exports);
+__exportStar(require('./dist/bundler/mod'), exports);
