3d5c0b927e456df63ffdaeef9e9c50b677341806
emil nordling <ekala001@gmail.com>
2020-11-22 00:02:56 +0100

    Fixes main.tsx in ui_kit

--- modules/ui_kit/main.tsx
+++ modules/ui_kit/main.tsx
@@ -1,19 +1,10 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ResetCss } from './';
-import { Home } from './doc/routes/home';
-import { Buttons } from './doc/routes/buttons';
+import { ResetCss } from './mod';
 
 ReactDOM.render(
   <Fragment>
     <ResetCss.h />
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home.h />} />
-        <Route path='/buttons' element={<Buttons.h />} />
-      </Routes>
-    </BrowserRouter>
   </Fragment>,
   document.getElementById('app'),
 );
