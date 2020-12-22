31e9ace630bfec8257e260eb0c370abcb5c4c6f5
emil nordling <ekala001@gmail.com>
2020-12-22 13:06:23 +0100

    Simplifies use_service logic

--- modules/instantiation/use_service.ts
+++ modules/instantiation/use_service.ts
@@ -1,19 +1,11 @@
-import { useEffect, useRef, useState } from 'react';
+import { useMemo } from 'react';
 import { Instantiation } from './instantiation';
 
 /**
  * Returns an instance, during the process all of its dependencies will also be created.
  */
 export function useService<T>(ctor: Instantiation.Ctor<T>): T {
-  const ref = useRef<Instantiation.Ctor<T>>(ctor);
-  const [service, setService] = useState(() => Instantiation.resolve(ctor));
-
-  useEffect(() => {
-    if (ref.current !== ctor) {
-      ref.current = ctor;
-      setService(() => Instantiation.resolve(ctor));
-    }
-  }, [ctor]);
+  const service = useMemo(() => Instantiation.resolve(ctor), [ctor]);
 
   return service;
 }
