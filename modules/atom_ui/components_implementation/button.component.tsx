0d15a03bc10fa0400fa64371a88dd123406b8874
emil nordling <ekala001@gmail.com>
2021-01-02 13:11:56 +0100

    Removes more namespacing

--- modules/atom_ui/components_implementation/button.component.tsx
+++ modules/atom_ui/components_implementation/button.component.tsx
@@ -4,64 +4,68 @@ import { BaseButton, OneAtomBaseButtonProps } from '../components_base/base_butt
 import { OneAtomCommonPropType } from '../prop_type';
 import { OneAtomSizeProps, Size } from './size.component';
 
-/**
- * Button
- */
+export interface OneAtomButtonProps extends OneAtomBaseButtonProps, OneAtomSizeProps, OneAtomCommonPropType {
+  round?: boolean;
+}
+
+const elementsShared = {
+  button: styled(BaseButton)`
+    border-radius: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    height: 30px;
+    min-width: 30px;
+    font-weight: 700;
+    padding: 0px 14px;
+    box-sizing: border-box;
+    font-size: 0.8125rem; // 13px
+    transition: background-color 0.3s ease;
+
+    &.round {
+      font-size: 16px;
+      padding: 0;
+    }
+  `,
+};
+
+const elements = {
+  actionButton: styled(elementsShared.button)`
+    color: var(--oa-button-action-clr, #ffffff);
+    background: var(--oa-button-action-bg, #0099ff);
+
+    &:hover:not(:disabled) {
+      background: var(--oa-button-action-bg-hover, #0088ff);
+    }
+
+    &:active:not(:disabled) {
+      background: var(--oa-button-action-bg-active, #0077ff);
+    }
+  `,
+  controlButton: styled(elementsShared.button)`
+    color: var(--oa-button-alt-clr, var(--oa-text-color, #ffffff));
+    background: var(--oa-button-alt-bg, #333333);
+
+    &:hover:not(:disabled) {
+      filter: brightness(90%) hue-rotate(2deg);
+    }
+
+    &:active:not(:disabled) {
+      filter: brightness(80%) hue-rotate(2deg);
+    }
+  `,
+};
+
 export namespace Button {
-  export interface Props extends OneAtomBaseButtonProps, OneAtomSizeProps, OneAtomCommonPropType {
-    round?: boolean;
-  }
-
-  const elements_shared = {
-    button: styled(BaseButton)`
-      border-radius: 15px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      position: relative;
-      height: 30px;
-      min-width: 30px;
-      font-weight: 700;
-      padding: 0px 14px;
-      box-sizing: border-box;
-      font-size: 0.8125rem; // 13px
-      transition: background-color 0.3s ease;
-
-      &.round {
-        font-size: 16px;
-        padding: 0;
-      }
-    `,
-  };
-
-  const elements = {
-    actionButton: styled(elements_shared.button)`
-      color: var(--oa-button-action-clr, #ffffff);
-      background: var(--oa-button-action-bg, #0099ff);
-
-      &:hover:not(:disabled) {
-        background: var(--oa-button-action-bg-hover, #0088ff);
-      }
-
-      &:active:not(:disabled) {
-        background: var(--oa-button-action-bg-active, #0077ff);
-      }
-    `,
-    controlButton: styled(elements_shared.button)`
-      color: var(--oa-button-alt-clr, var(--oa-text-color, #ffffff));
-      background: var(--oa-button-alt-bg, #333333);
-
-      &:hover:not(:disabled) {
-        filter: brightness(90%) hue-rotate(2deg);
-      }
-
-      &:active:not(:disabled) {
-        filter: brightness(80%) hue-rotate(2deg);
-      }
-    `,
-  };
-
-  export const action: FC<Props> = function OneAtom_Button_action({ children, fluid, className, round, type = 'action', ...rest }) {
+  export const action: FC<OneAtomButtonProps> = function OneAtom_Button_action({
+    children,
+    fluid,
+    className,
+    round,
+    type = 'action',
+    ...rest
+  }) {
     const cls_name = `${className ?? ''} ${round ? 'round' : ''}`;
 
     return (
@@ -73,7 +77,14 @@ export namespace Button {
     );
   };
 
-  export const alt: FC<Props> = function OneAtom_Button_control({ children, fluid, className, round, type = 'action', ...rest }) {
+  export const alt: FC<OneAtomButtonProps> = function OneAtom_Button_control({
+    children,
+    fluid,
+    className,
+    round,
+    type = 'action',
+    ...rest
+  }) {
     const cls_name = `${className ?? ''} ${round ? 'round' : ''}`;
 
     return (
@@ -85,12 +96,12 @@ export namespace Button {
     );
   };
 
-  export const h: FC<Props> = function OneAtom_Button({ children, fluid, className, type = 'action', ...rest }) {
+  export const h: FC<OneAtomButtonProps> = function OneAtom_Button({ children, fluid, className, type = 'action', ...rest }) {
     return (
       <Size fluid={fluid}>
-        <elements_shared.button className={className ?? ''} {...rest}>
+        <elementsShared.button className={className ?? ''} {...rest}>
           {children}
-        </elements_shared.button>
+        </elementsShared.button>
       </Size>
     );
   };
--- modules/atom_ui/components_implementation/text.component.tsx
+++ modules/atom_ui/components_implementation/text.component.tsx
@@ -2,98 +2,95 @@
 import styled from 'styled-components';
 import { OneAtomCommonPropType } from '../prop_type';
 
-/**
- * Text
- */
+export interface OneAtomTextProps extends OneAtomCommonPropType {}
+
+interface PropsForCustom extends OneAtomTextProps {
+  size?: number;
+  spacing?: number;
+  weight?: number;
+}
+
+const elements = {
+  text: styled.div`
+    color: var(--oa-text-color, inherit);
+    transition: color 250ms ease;
+    display: contents;
+
+    strong {
+      color: var(--oa-text-strong, #09f);
+      letter-spacing: 0;
+    }
+
+    b,
+    strong {
+      font-weight: 600;
+    }
+
+    a {
+      word-wrap: break-word;
+      color: var(--oa-text-link, #09f);
+
+      &:link,
+      &:visited {
+        text-decoration: none;
+      }
+    }
+  `,
+  p: styled.p`
+    font-size: var(--oa-text-p-size, 1rem);
+    line-height: var(--oa-text-p-line-height, 2rem);
+    font-weight: var(--oa-text-p-weight, 400);
+    letter-spacing: var(--oa-text-p-letter-spacing, normal);
+  `,
+  h1Plus: styled.h1`
+    color: var(--oa-text-color-heading, #1b2124);
+    font-size: var(--oa-text-h1plus-size, 4rem);
+    line-height: var(--oa-text-h1plus-line-height, 4.5rem);
+    font-weight: var(--oa-text-h1plus-weight, 700);
+    letter-spacing: var(--oa-text-h1plus-weight, -1.5);
+  `,
+  h1: styled.h1`
+    color: var(--oa-text-color-heading, #1b2124);
+    font-size: var(--oa-text-h1plus-size, 2.5rem);
+    line-height: var(--oa-text-h1plus-line-height, 2.4375rem);
+    font-weight: var(--oa-text-h1plus-weight, 700);
+    letter-spacing: var(--oa-text-h1plus-weight, -1.5);
+  `,
+  h2: styled.h2`
+    color: var(--oa-text-color-heading, #1b2124);
+    font-size: var(--oa-text-h2-size, 2rem);
+    line-height: var(--oa-text-h2-line-height, 2.5rem);
+    font-weight: var(--oa-text-h2-weight, 700);
+    letter-spacing: var(--oa-text-h2-letter-spacing, -0.5);
+  `,
+  h3: styled.h3`
+    color: var(--oa-text-color-heading, #1b2124);
+    font-size: var(--oa-text-h3-size, 1.5rem);
+    line-height: var(--oa-text-h3-line-height, 2rem);
+    font-weight: var(--oa-text-h3-weight, normal);
+    letter-spacing: var(--oa-text-h3-letter-spacing, normal);
+  `,
+  h4: styled.h4`
+    color: var(--oa-text-color-heading, #1b2124);
+    font-size: var(--oa-text-h4-size, 1rem);
+    line-height: var(--oa-text-h4-line-height, 1.5rem);
+    font-weight: var(--oa-text-h4-weight, 700);
+    letter-spacing: var(--oa-text-h4-letter-spacing, 0.25);
+  `,
+  custom: styled.span<PropsForCustom>`
+    font-size: ${({ size }) => size ?? 1}rem;
+    letter-spacing: ${({ spacing }) => spacing ?? 1.7}rem;
+    font-weight: ${({ weight }) => weight ?? 500};
+    display: inline-flex;
+  `,
+};
+
 export namespace Text {
-  export type Props = OneAtomCommonPropType;
-
-  interface PropsForCustom extends Props {
-    size?: number;
-    spacing?: number;
-    weight?: number;
-  }
-
-  const elements = {
-    text: styled.div`
-      color: var(--oa-text-color, inherit);
-      transition: color 250ms ease;
-      display: contents;
-
-      strong {
-        color: var(--oa-text-strong, #09f);
-        letter-spacing: 0;
-      }
-
-      b,
-      strong {
-        font-weight: 600;
-      }
-
-      a {
-        word-wrap: break-word;
-        color: var(--oa-text-link, #09f);
-
-        &:link,
-        &:visited {
-          text-decoration: none;
-        }
-      }
-    `,
-    p: styled.p`
-      font-size: var(--oa-text-p-size, 1rem);
-      line-height: var(--oa-text-p-line-height, 2rem);
-      font-weight: var(--oa-text-p-weight, 400);
-      letter-spacing: var(--oa-text-p-letter-spacing, normal);
-    `,
-    h1Plus: styled.h1`
-      color: var(--oa-text-color-heading, #1b2124);
-      font-size: var(--oa-text-h1plus-size, 4rem);
-      line-height: var(--oa-text-h1plus-line-height, 4.5rem);
-      font-weight: var(--oa-text-h1plus-weight, 700);
-      letter-spacing: var(--oa-text-h1plus-weight, -1.5);
-    `,
-    h1: styled.h1`
-      color: var(--oa-text-color-heading, #1b2124);
-      font-size: var(--oa-text-h1plus-size, 2.5rem);
-      line-height: var(--oa-text-h1plus-line-height, 2.4375rem);
-      font-weight: var(--oa-text-h1plus-weight, 700);
-      letter-spacing: var(--oa-text-h1plus-weight, -1.5);
-    `,
-    h2: styled.h2`
-      color: var(--oa-text-color-heading, #1b2124);
-      font-size: var(--oa-text-h2-size, 2rem);
-      line-height: var(--oa-text-h2-line-height, 2.5rem);
-      font-weight: var(--oa-text-h2-weight, 700);
-      letter-spacing: var(--oa-text-h2-letter-spacing, -0.5);
-    `,
-    h3: styled.h3`
-      color: var(--oa-text-color-heading, #1b2124);
-      font-size: var(--oa-text-h3-size, 1.5rem);
-      line-height: var(--oa-text-h3-line-height, 2rem);
-      font-weight: var(--oa-text-h3-weight, normal);
-      letter-spacing: var(--oa-text-h3-letter-spacing, normal);
-    `,
-    h4: styled.h4`
-      color: var(--oa-text-color-heading, #1b2124);
-      font-size: var(--oa-text-h4-size, 1rem);
-      line-height: var(--oa-text-h4-line-height, 1.5rem);
-      font-weight: var(--oa-text-h4-weight, 700);
-      letter-spacing: var(--oa-text-h4-letter-spacing, 0.25);
-    `,
-    custom: styled.span<PropsForCustom>`
-      font-size: ${({ size }) => size ?? 1}rem;
-      letter-spacing: ${({ spacing }) => spacing ?? 1.7}rem;
-      font-weight: ${({ weight }) => weight ?? 500};
-      display: inline-flex;
-    `,
-  };
-
-  export const h: FC<Props> = function OneAtom_Text({ children, className }) {
+  export const h: FC<OneAtomTextProps> = function OneAtom_Text({ children, className }) {
     return <elements.text className={className}>{children}</elements.text>;
   };
 
-  export const h1_plus: FC<Props> = function OneAtom_Text_h1_plus({ children, className }) {
+  export const h1_plus: FC<OneAtomTextProps> = function OneAtom_Text_h1_plus({ children, className }) {
     return (
       <Text.h className={className}>
         <elements.h1Plus>{children}</elements.h1Plus>
@@ -101,7 +98,7 @@ export namespace Text {
     );
   };
 
-  export const h1: FC<Props> = function OneAtom_Text_h1({ children, className }) {
+  export const h1: FC<OneAtomTextProps> = function OneAtom_Text_h1({ children, className }) {
     return (
       <Text.h className={className}>
         <elements.h1>{children}</elements.h1>
@@ -109,7 +106,7 @@ export namespace Text {
     );
   };
 
-  export const h2: FC<Props> = function OneAtom_Text_h2({ children, className }) {
+  export const h2: FC<OneAtomTextProps> = function OneAtom_Text_h2({ children, className }) {
     return (
       <Text.h className={className}>
         <elements.h2>{children}</elements.h2>
@@ -117,7 +114,7 @@ export namespace Text {
     );
   };
 
-  export const h3: FC<Props> = function OneAtom_Text_h3({ children, className }) {
+  export const h3: FC<OneAtomTextProps> = function OneAtom_Text_h3({ children, className }) {
     return (
       <Text.h className={className}>
         <elements.h3>{children}</elements.h3>
@@ -125,7 +122,7 @@ export namespace Text {
     );
   };
 
-  export const h4: FC<Props> = function OneAtom_Text_h4({ children, className }) {
+  export const h4: FC<OneAtomTextProps> = function OneAtom_Text_h4({ children, className }) {
     return (
       <Text.h className={className}>
         <elements.h4>{children}</elements.h4>
@@ -133,7 +130,7 @@ export namespace Text {
     );
   };
 
-  export const body: FC<Props> = function OneAtom_Text_h4({ children, className }) {
+  export const body: FC<OneAtomTextProps> = function OneAtom_Text_h4({ children, className }) {
     return (
       <Text.h className={className}>
         <elements.p>{children}</elements.p>
